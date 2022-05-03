74887c113e3dc9db9a50e71a2e63967e40f6c5f3
MartinCba <martinalejandrohernandez1@gmail.com>
2022-05-03 16:42:32 -0300

    validacion del formulario

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,37 @@
 import Input from "./Input";
 
 function Login() {
+  const submitHandle = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    console.log(regexEmail.test(email));
+
+    if (email === "" || password === "") {
+      console.log("los campos no pueden estar vacios");
+      return;
+    }
+
+    if (email !== "" && !regexEmail.test(email)) {
+      console.log("debes escribir una dirección de correo electronico válida");
+      return;
+    }
+
+    if (email !== "challenge@alkemy.com" || password !== "react") {
+      console.log("Credenciales inválidas");
+      return;
+    }
+    console.log("ok, información lista para ser enviada");
+  };
+
   return (
     <>
       <h2>Formulario de Login</h2>
-      <form>
-        <Input label="Correo electronico :" type="email" name="email" />
+      <form onSubmit={submitHandle}>
+        <Input label="Correo electronico :" type="text" name="email" />
         <br />
         <Input label="Contraseña :" type="password" name="password" />
         <br />
